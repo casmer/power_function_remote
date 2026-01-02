8dc5522834e7855abc5905513ba29678b681411b
Casey Gregoire <caseyg@lalosoft.com>
2026-01-01 21:13:41 -0500

    hub emulator

--- src/main.cpp
+++ src/main.cpp
@@ -1,179 +1,80 @@
-/// @copyright  Copyright (c) 2025 Casey Gregoire
-///             Released under MIT License
+/**
+ * A Legoino example to emulate a train hub which directly sends
+ * out the motor commands as IR signals to a power function IR receiver. 
+ * With this setup you can upgrade your power function systems to powerup systems
+ * 
+ * For the setup the IR LED has to be connected on the OUTPUT PIN 12 of the 
+ * ESP controller. This will work out of the Box with the M5 Atom matrix/light
+ * esp32 board which has a build in IR LED on Port 12
+ * 
+ * Port A of the powered up hub is mapped to the RED power function port
+ * Port B of the powered up hub is mapped to the BLUE power function port
+ * 
+ * Example video: https://www.youtube.com/watch?v=RTNexxT4-yQ&t=16s
+ * 
+ * (c) Copyright 2020 - Cornelius Munz
+ * Released under MIT License
+ * 
+ */
 
-#include <Arduino.h>
+#include "Lpf2HubEmulation.h"
 #include "PowerFunctions.h"
-#include <millisDelay.h>
-#include "utilities.hpp"
+#include "LegoinoCommon.h"
 
-/// @brief Delay between prints to Serial and no values have changed
-static constexpr unsigned long PRINT_DELAY_MS = 3000;
-/// @brief Delay object for printing
-millisDelay printDelay;
+// create a hub instance
+Lpf2HubEmulation myEmulatedHub("TrainHub", HubType::POWERED_UP_HUB);
 
-int analogXPin = A6; // Analog pin connected to a potentiometer
-int analogYPin = A7; // Analog pin connected to a potentiometer
-int invertJoystickXPin = A1; // Analog pin to read if X axis should be inverted
-int invertJoystickYPin = A2; // Analog pin to read if Y axis should be inverted
-bool invertJoystickX = false;
-bool invertJoystickY = false;
-int xValue = 0;      // Variable to store the read value
-int yValue = 0;      // Variable to store the read value
+// create a power functions instance (IR LED on Pin 12, IR Channel 0)
+PowerFunctions pf(12, 0);
 
-int xValueScaled = 0; // Variable to store the scaled read value
-int yValueScaled = 0; // Variable to store the scaled read value
 
-int leftSpeed = 50;  // initial speed
-int rightSpeed = 50; // initial speed
+void writeValueCallback(byte port, byte value)
+{
+  Serial.println("writeValueCallback: ");
+  Serial.println(port, HEX);
+  Serial.println(value, HEX);
 
-// create a power functions instance
-PowerFunctions powerFunctions(12, 3); // Pin 12, Channel 3
+  if (port == 0x00)
+  {
+    pf.single_pwm(PowerFunctionsPort::RED, pf.speedToPwm(value));    
+  }
+
+  if (port == 0x01)
+  {
+    pf.single_pwm(PowerFunctionsPort::BLUE, pf.speedToPwm(value));
+  }
+
+  if (port == 0x32)
+  {
+    Serial.print("Hub LED command received with color: ");
+    Serial.println(LegoinoCommon::ColorStringFromColor(value).c_str());
+  }
+}
 
 void setup()
 {
-  printDelay.start(PRINT_DELAY_MS);
   Serial.begin(115200);
-  pinMode(invertJoystickXPin, INPUT_PULLUP);
-  pinMode(invertJoystickYPin, INPUT_PULLUP);
-}
-
-
-void PrintSpeed(int speed)
-{
-  switch (static_cast<PowerFunctionsPwm>(speed))
-  {
-  case PowerFunctionsPwm::FLOAT:
-    Serial.print("FLOAT");
-    break;
-  case PowerFunctionsPwm::BRAKE:
-    Serial.print("BRAKE");
-    break;
-  case PowerFunctionsPwm::FORWARD1:
-    Serial.print("FORWARD1");
-    break;
-  case PowerFunctionsPwm::FORWARD2:
-    Serial.print("FORWARD2");
-    break;
-  case PowerFunctionsPwm::FORWARD3:
-    Serial.print("FORWARD3");
-    break;
-  case PowerFunctionsPwm::FORWARD4:
-    Serial.print("FORWARD4");
-    break;
-  case PowerFunctionsPwm::FORWARD5:
-    Serial.print("FORWARD5");
-    break;
-  case PowerFunctionsPwm::FORWARD6:
-    Serial.print("FORWARD6");
-    break;
-  case PowerFunctionsPwm::FORWARD7:
-    Serial.print("FORWARD7");
-    break;
-  case PowerFunctionsPwm::REVERSE1:
-    Serial.print("REVERSE1");
-    break;
-  case PowerFunctionsPwm::REVERSE2:
-    Serial.print("REVERSE2");
-    break;
-  case PowerFunctionsPwm::REVERSE3:
-    Serial.print("REVERSE3");
-    break;
-  case PowerFunctionsPwm::REVERSE4:
-    Serial.print("REVERSE4");
-    break;
-  case PowerFunctionsPwm::REVERSE5:
-    Serial.print("REVERSE5");
-    break;
-  case PowerFunctionsPwm::REVERSE6:
-    Serial.print("REVERSE6");
-    break;
-  case PowerFunctionsPwm::REVERSE7:
-    Serial.print("REVERSE7");
-    break;
-  default:
-    Serial.print(static_cast<int>(speed));
-    break;
-  }
-}
-
-void printValues()
-{
-  static int last_xValueScaled = 0;
-  static int last_yValueScaled = 0;
-
-  if (printDelay.justFinished() || last_xValueScaled != xValueScaled || last_yValueScaled != yValueScaled)
-  {
-    last_xValueScaled = xValueScaled;
-    last_yValueScaled = yValueScaled;
-
-    Serial.print("X");
-    if (invertJoystickX)
-    {
-      Serial.print("(inverted): ");
-    }
-    else
-    {
-      Serial.print(": ");
-    }
-    Serial.print(xValue);
-    Serial.print(" | Scaled X: ");
-    Serial.print(xValueScaled);
-    Serial.print(" | Y");
-    if (invertJoystickY)
-    {
-      Serial.print("(inverted): ");
-    }
-    else
-    {
-      Serial.print(": ");
-    }
-    Serial.print(yValue);
-    Serial.print(" | Scaled Y: ");
-    Serial.println(yValueScaled);
-
-    Serial.print("Left Speed: [");
-    Serial.print(leftSpeed);
-    Serial.print("] ");
-    PrintSpeed(speedToPowerFunctionsPwm(leftSpeed));
-    // Serial.print( static_cast<int>(leftSpeed));
-    Serial.print(" | Right Speed: [");
-    Serial.print(rightSpeed);
-    Serial.print("] ");
-    PrintSpeed(speedToPowerFunctionsPwm(rightSpeed));
-    Serial.println();
-    // Serial.println(static_cast<int>(rightSpeed));
-  }
-  if (!printDelay.isRunning())
-  {
-    printDelay.start(PRINT_DELAY_MS);
-  }
+  // define the callback function if a write message event on the characteristic occurs
+  myEmulatedHub.setWritePortCallback(&writeValueCallback); 
+  myEmulatedHub.start();
 }
 
 // main loop
 void loop()
 {
 
-  invertJoystickX = digitalRead(invertJoystickXPin) == LOW;
-  invertJoystickY = digitalRead(invertJoystickYPin) == LOW;
-  xValue = analogRead(analogXPin); // read the input pin
-  if (invertJoystickX)
+  // if an app is connected, attach some devices on the ports to signalize 
+  // the app that values could be received/written to that ports
+  if (myEmulatedHub.isConnected && !myEmulatedHub.isPortInitialized)
   {
-    xValue = 1023 - xValue;
+    delay(1000);
+    myEmulatedHub.isPortInitialized = true;
+    myEmulatedHub.attachDevice((byte)PoweredUpHubPort::A, DeviceType::TRAIN_MOTOR);
+    delay(1000);
+    myEmulatedHub.attachDevice((byte)PoweredUpHubPort::LED, DeviceType::HUB_LED);
+    delay(1000);
+    myEmulatedHub.attachDevice((byte)PoweredUpHubPort::B, DeviceType::TRAIN_MOTOR);
+    delay(1000);
   }
-  yValue = analogRead(analogYPin); // read the input pin
-  if (invertJoystickY)
-  {
-    yValue = 1023 - yValue;
-  }
-  xValueScaled = rampFunction(scaleAnalogValue(xValue));
-  yValueScaled = rampFunction(scaleAnalogValue(yValue));
-  translateJoystickToSpeed(leftSpeed, rightSpeed, xValueScaled, yValueScaled);
- leftSpeed = -leftSpeed; // Invert left motor
-  printValues();
-
-  powerFunctions.combo_pwm(
-      static_cast<PowerFunctionsPwm>(speedToPowerFunctionsPwm(leftSpeed)),
-      static_cast<PowerFunctionsPwm>(speedToPowerFunctionsPwm(rightSpeed)));
-  delay(30);
 
 } // End of loop
